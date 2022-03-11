3359f0cef050d54d8dcf8fb3f2e8bfb2ef639728
Jake Eissler <jakeeissler1@gmail.com>
2022-03-11 07:21:55 -0700

    having problems with the api route. commented it out for now and created the helper function for random numbers

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const fs = require("fs");
 const PORT = process.env.PORT || 9420;
 const app = express();
 
+//array for note objects in db.json
+let notes = [];
+
 //middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,6 +25,18 @@ app.get("/", (req, res) =>
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
+
+// app.get("/api/notes", (req, res) =>
+//   res.sendFile(
+//     path.join(__dirname, "./db/db.json", "utf-8", (err, data) => {
+//       if (err) {
+//         console.log(err, "dis be da problem");
+//       }
+//       const parsedNotes = JSON.parse(data);
+//       res.sendFile(parsedNotes);
+//     })
+//   )
+// );
 //listen
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
