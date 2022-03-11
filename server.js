47fe24201aac0d0eddd43b72fcf8bed7586f1c77
Jake Eissler <jakeeissler1@gmail.com>
2022-03-11 06:53:46 -0700

    installed dependencies and created the server at server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+//----------unique id's
+//const uuid = require("./helpers/uuid");
+const PORT = process.env.PORT || 9420;
+const app = express();
+
+//middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//app.use('/api', api)
+
+app.use(express.static("public"));
+
+//GET route for home page
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+//GET Route for notes page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
+//listen
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
